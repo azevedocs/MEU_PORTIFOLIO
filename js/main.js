83bb73b70e862427f8e891d79f21104d59b2e804
azevedocs <60318636+azevedocs@users.noreply.github.com>
2020-07-19 23:29:29 -0300

    Finalizando Slider Slick

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
